316478f25b87e99d8534018d32faaac007ef99be
rajvardhan97 <n.rajvardhan2627@gmail.com>
2022-05-22 19:02:43 +0530

    Create Linked List by adding elements

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List linkedlist = new List();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.Display();
+        }
+    }
+}
+
